08873afc71a7ece1004c8d3bbad17c216d01c50a
ByungseokJeong <newfuture@gmail.com>
2023-04-07 00:28:18 +0900

    chapter 2-1

---
+++ practice 2-1py.py
@@ -0,0 +1,35 @@
+def MH():
+    print("1 + 2 + 3 + 4 + 5 = ", 1 + 2 + 3 + 4 + 5)
+    print("Simple is the best!")
+    print("행복한 파이썬~")
+
+MH()
+MH()    
+
+    
+def jbs(x):
+    print(x)
+
+jbs(2)
+jbs("a+b")
+jbs("나는 자연인이다")
+
+
+def kick(o, k):
+    print((o+k) / 2)
+
+kick(2, 4)
+
+
+def sorry(mo):
+    return -mo
+    
+dick = sorry(3)
+print(dick)    
+
+
+def bingbong(xi, xu):
+    return ((xi + xu) / 2)
+
+coin = bingbong(6, 4)
+print(coin)
