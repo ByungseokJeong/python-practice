e79569c8de5aefac948974fec23be47faa518f2a
ByungseokJeong <newfuture@gmail.com>
2023-04-07 22:28:08 +0900

    주석, 메인함수 정의

---
+++ practice 2-2.py
@@ -0,0 +1,29 @@
+def adder(n1, n2):
+    return n1 + n2
+
+def main():
+    print(adder(5, 7))
+    print(adder(2, 3))
+    print(adder("abc", "def"))
+
+main()
+
+
+# practice 2-2.py
+def add(num1, num2):
+    return num1 + num2
+def min(num1, num2):
+    return num1 - num2
+def mul(num1, num2):
+    return num1 * num2
+def div (num1, num2):
+    return num1 / num2
+
+
+def main():
+    print(add(5, 3))
+    print(min(5, 3))
+    print(mul(5, 3))
+    print(div(5, 3))
+
+main()
