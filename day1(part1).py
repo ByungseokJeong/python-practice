af344c35d167d8ceded3c0ea2c81d720ce884cc5
ByungseokJeong <newfuture0401@gmail.com>
2023-04-20 00:00:48 +0900

    챕터 1 정리

---
+++ day1(part1).py
@@ -0,0 +1,43 @@
+print(33 + 55)
+print((9 * 2) / 2) # 숫자열 출력
+print("hello, world") # 문자열 출력
+print(2 * 5 / 4)
+print("3 + 5") # 3 + 5를 하나의 문자열로 인식
+print(1, 2, 3, "Hello", "world!")
+v1 = 25
+v2 = 30
+print(v1 + v2) #메모리 공간에 저장된 v1과 v2의 값 출력
+
+# ex-1
+print("안녕하세요")
+print(1+2+3+4+5+6+7+8+9+10)
+print(2 * 2 * 2 * 2 * 2)
+print("5-(3-1) =" , 3)
+print(10 / 2 + 10 * 2)
+
+# ex-2
+x = 2 * 2 * 2
+print(x)
+y = x / 4
+print(y)
+z = y * y
+print(z)
+
+x = 100
+print(x)
+x = 200
+print(x) # 앞에 저장된 값 100 삭제
+
+
+# ex-3
+x = 2 * 2 * 2
+print(x)
+x = x / 4 # 앞서 저장된 x의 값 8 대입
+print(x)
+x = x * x # 앞서 저장된 x의 값 2 대입
+print(x)
+
+x, y = 0, 797
+x = y # 대입연산자는 오른쪽에 저장된 값을 왼쪽에 저장함
+print(x)
+
