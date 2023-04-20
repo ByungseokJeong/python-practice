264c7d14cab017fac2c40f73fec715a5456b3da7
ByungseokJeong <newfuture0401@gmail.com>
2023-04-21 02:18:48 +0900

    3과 input과 eval 함수

---
+++ day2(part3).py
@@ -0,0 +1,21 @@
+n1 = input("?")
+print(n1)
+n2 = input("?    ")
+print(n2)
+n3 = n1 + n2
+print(n3) # 입력받은 값을 문자열로 출력
+
+# 연습문제 3-1
+sim = input("첫 번째 입력: ")
+yim = input("두번째 입력: ")
+print("두 입력의 합:", sim + yim)
+
+rad = eval(input("radius: "))
+area = rad * rad * 3.14
+print(area)
+
+# 연습문제 3-2
+xio = eval(input("첫 번째 입력: "))
+yio = eval(input("두 번째 입력: "))
+print("두 입력의 합:", xio + yio )
+                 
